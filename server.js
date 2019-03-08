87c57c870086c635dba08e65cf6943254d3bec14
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-08 09:53:10 -0700

    revise PORT env on server

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ server = express();
 server.use('/app', express.static(path.join(__dirname, 'app/build')));
 server.use('/', express.static(path.join(__dirname, 'landing')));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Listening on ${port}`));
