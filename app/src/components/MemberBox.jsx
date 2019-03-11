fa94c247acb79ecca6735bfbcce28687e112c30b
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-11 11:10:07 -0400

    Created basic MemberBox component scaffolding and import

---
+++ app/src/components/MemberBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MemberBox extends Component {
+  render() {
+    return (
+      <div>testing</div>
+    )
+  }
+}
+
+export default MemberBox;
--- app/src/views/AppView.jsx
+++ app/src/views/AppView.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Route } from "react-router";
 import SideBar from "../components/SideBar/SideBar";
 import Group from "../components/Group";
+import MemberBox from "../components/MemberBox";
 const StyledAppView = styled.div`
   display: flex;
 `;
@@ -11,6 +12,7 @@ export default class AppView extends React.Component {
     return (
       <StyledAppView>
         <SideBar />
+        <MemberBox />
         <Route path={`/app/:group`} component={Group} />
       </StyledAppView>
     );
