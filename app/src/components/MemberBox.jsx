c9a455ce9913bc026de85c11d4a73ba52b2c74de
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-11 13:14:16 -0400

    Checkboxes now update state

--- app/src/components/MemberBox.jsx
+++ app/src/components/MemberBox.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
 const groupMembers = [
-  {name: 'Frodo Baggins', phone: 123, isChecked: 'false'}, 
-  {name: 'Peregrin  Took', phone: 1234, isChecked: 'false'}, 
-  {name: 'Meriadoc Brandybuck', phone: 12345, isChecked: 'false'}, {name: 'Samwise Gamgee', phone: 123456, isChecked: 'false'}
+  {name: 'Frodo Baggins', phone: 123, isChecked: false}, 
+  {name: 'Peregrin  Took', phone: 1234, isChecked: false}, 
+  {name: 'Meriadoc Brandybuck', phone: 12345, isChecked: false}, {name: 'Samwise Gamgee', phone: 123456, isChecked: false}
 ]
 
 class MemberBox extends Component {
@@ -20,6 +20,20 @@ class MemberBox extends Component {
     })
   }
 
+  toggle = (phone) => {
+    let updatedMembers = [...this.state.members];
+
+    updatedMembers.map(member => {
+      (member.phone === phone &&
+        (member.isChecked = !member.isChecked) 
+      )
+    })
+
+    this.setState({
+      members : updatedMembers,
+    })
+  }
+
   render() {
     return (
       <div>
@@ -27,12 +41,21 @@ class MemberBox extends Component {
         <div>
           {this.state.members.map((member, index) => 
             <span>
-              <input key={index} type="checkbox" />
-              <label>{member.name}</label>
+              <input 
+                key={index} 
+                type="checkbox" 
+                onClick={() => this.toggle(member.phone)} />
+              <label>{member.name}:{JSON.stringify(member.isChecked)}</label>
             </span>
           )}
           <button>
             add member
+          </button>
+          <button>
+            select all
+          </button>
+          <button>
+            select none
           </button>
         </div>
       </div>
