51d410c415f5ded90b9fa1092cd2c76e40f0982e
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-11 17:54:19 -0600

    fix /app basename in dev vs prod

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import { Route } from "react-router";
-import LoginView from "./views/LoginView";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import LoginView from './views/LoginView';
 import SignUpView from './views/SignUpView';
-import AppView from './views/AppView'
-import styled from 'styled-components'
+import AppView from './views/AppView';
+import styled from 'styled-components';
 
-const StyledApp = styled.div`
-
-`
+const StyledApp = styled.div``;
 class App extends Component {
   render() {
     return (
       <StyledApp>
-        <Route path='/login' component={LoginView} />
-        <Route path='/signup' component={SignUpView} />
-        <Route path='/app' component={AppView} />
+        <Switch>
+          <Route path="/login" component={LoginView} />
+          <Route path="/signup" component={SignUpView} />
+          <Route path="/" component={AppView} />
+        </Switch>
       </StyledApp>
     );
   }
