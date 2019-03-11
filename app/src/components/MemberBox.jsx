9ea84a0583fe96f68f3a63d8b636b1d7f35183aa
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-11 12:20:57 -0400

    Added mock group members and building checkbox functionality

--- app/src/components/MemberBox.jsx
+++ app/src/components/MemberBox.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 
+const groupMembers = [
+  {name: 'Frodo Baggins', phone: 123, isChecked: 'false'}, 
+  {name: 'Peregrin  Took', phone: 1234, isChecked: 'false'}, 
+  {name: 'Meriadoc Brandybuck', phone: 12345, isChecked: 'false'}, {name: 'Samwise Gamgee', phone: 123456, isChecked: 'false'}
+]
+
 class MemberBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      members: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      members: groupMembers,
+    })
+  }
+
   render() {
     return (
-      <div>testing</div>
+      <div>
+        <p>Your group members will appear here. Uncheck the box to remove someone from the message chain.</p>
+        <div>
+          {this.state.members.map((member, index) => 
+            <span>
+              <input key={index} type="checkbox" />
+              <label>{member.name}</label>
+            </span>
+          )}
+          <button>
+            add member
+          </button>
+        </div>
+      </div>
     )
   }
 }
 
-export default MemberBox;
+export default MemberBox
