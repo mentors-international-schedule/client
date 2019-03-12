36b4682778511564d5123644d590f41a741565db
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-12 17:33:14 -0400

    Created action creators for member box

--- app/src/actions/memberBoxActions.js
+++ app/src/actions/memberBoxActions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import URL from "./AJAX_URL";
 
 import {
   GETTING_CONTACTS,
@@ -8,3 +9,39 @@ import {
   FAIL_CREATING_CONTACT,
   SUCCESS_CREATING_CONTACT
 } from "./actionTypes";
+
+export const getContacts = (groupId) = dispatch => {
+  dispatch({ type: GETTING_CONTACTS });
+  axios
+    .get(`${URL}api/contacts/:id`, { groupId })
+    .then(res => {
+      dispatch({
+        type: SUCCESS_GETTING_CONTACTS,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch({
+        type: FAIL_GETTING_CONTACTS,
+        payload: err
+      })
+    );
+}
+
+export const createContact = (name, phone, groupId) = dispatch => {
+  dispatch({ type: CREATING_CONTACT });
+  axios
+    .get(`${URL}api/contacts`, { name, phone, groupId })
+    .then(res => {
+      dispatch({
+        type: SUCCESS_CREATING_CONTACT                                                                                                                                                                                                                                                                                                                                                                                           ,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch({
+        type: FAIL_CREATING_CONTACT,
+        payload: err
+      })
+    );
+}
