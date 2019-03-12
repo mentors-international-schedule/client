0e6957736afad7e4a9aecb3002b06a6ba5907a8e
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-11 20:35:22 -0400

    Added callback to login action creator

--- app/src/actions/loginActions.js
+++ app/src/actions/loginActions.js
@@ -3,7 +3,7 @@ import URL from './AJAX_URL'
 import { LOGGING_IN, SUCCESS_LOGIN, FAIL_LOGIN } from "./actionTypes";
 
 
-export const login = (email, password) => dispatch => {
+export const login = (email, password, callback) => dispatch => {
   dispatch({ type: LOGGING_IN });
   axios
     .post(`${URL}api/users/login`, { email,password })
@@ -13,6 +13,9 @@ export const login = (email, password) => dispatch => {
         type:SUCCESS_LOGIN, 
         payload:res.data,
       })
+      if(callback && typeof callback === 'function') {
+        callback(res.data.token)
+      }
     })
     .catch(err => 
       dispatch({
