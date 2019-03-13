132434b4646e4edb0811275b92f372999e1ab7ca
conconrob <connortprobertson@gmail.com>
2019-03-13 17:46:41 +0000

    Connected send messages

--- app/src/actions/messageActions.js
+++ app/src/actions/messageActions.js
@@ -7,7 +7,7 @@ import {
   FAIL_SENDING_MESSAGE,
   SUCCESS_GETTING_MESSAGES,
   FAIL_GETTING_MESSAGES,
-  GETING_MESSAGES,
+  GETTING_MESSAGES,
   DRAFTING_MESSAGE,
   SUCCESS_DRAFTING_MESSAGE,
   FAIL_DRAFTING_MESSAGE
@@ -15,8 +15,9 @@ import {
 
 export const sendMessage = (contact_ids, message, group_id) => dispatch => {
   dispatch({ type: SENDING_MESSAGE });
+  debugger
   axios
-    .post(`${URL}notifications:${group_id}`, { message, contact_ids })
+    .post(`${URL}api/notifications/${group_id}`, { message, contact_ids })
     .then(res => dispatch({ type: SUCCESS_SENDING_MESSAGE, payload: res.data }))
     .catch(err => dispatch({ type: FAIL_SENDING_MESSAGE, payload: err }));
 };
@@ -24,14 +25,14 @@ export const sendMessage = (contact_ids, message, group_id) => dispatch => {
 export const draftMessage = (message, group_id) => dispatch => {
   dispatch({ type: DRAFTING_MESSAGE });
   axios
-    .post(`${URL}notifications/drafts:${group_id}`, { message })
+    .post(`${URL}api/notifications/drafts/${group_id}`, { message })
     .then(res => dispatch({ type: SUCCESS_DRAFTING_MESSAGE }))
     .catch(err => dispatch({ type: FAIL_DRAFTING_MESSAGE, payload: err }));
 };
 
 export const getMessages = (group_id) => dispatch => {
-  dispatch({type:GETING_MESSAGES});
-  axios.get(`${URL}notifications:${group_id}`)
+  dispatch({type:GETTING_MESSAGES});
+  axios.get(`${URL}api/notifications/${group_id}`)
     .then(res => dispatch({type:SUCCESS_GETTING_MESSAGES, payload: res.data } ))
     .catch(err =>dispatch({type:FAIL_GETTING_MESSAGES, payload:err} ) )
 }
--- app/src/reducers/messageReducer.js
+++ app/src/reducers/messageReducer.js
@@ -1,36 +1,47 @@
 import {
-    SENDING_MESSAGE,
-    SUCCESS_SENDING_MESSAGE,
-    FAIL_SENDING_MESSAGE,
-    SUCCESS_GETTING_MESSAGES,
-    FAIL_GETTING_MESSAGES,
-    GETING_MESSAGES,
-    DRAFTING_MESSAGE,
-    SUCCESS_DRAFTING_MESSAGE,
-    FAIL_DRAFTING_MESSAGE,
-    GETTING_MESSAGES
-  } from "../actions/actionTypes";
+  SENDING_MESSAGE,
+  SUCCESS_SENDING_MESSAGE,
+  FAIL_SENDING_MESSAGE,
+  SUCCESS_GETTING_MESSAGES,
+  FAIL_GETTING_MESSAGES,
+  DRAFTING_MESSAGE,
+  SUCCESS_DRAFTING_MESSAGE,
+  FAIL_DRAFTING_MESSAGE,
+  GETTING_MESSAGES
+} from "../actions/actionTypes";
 
-  const initState = {
-      messages : [],
-      sendingMessage: false,
-      gettingMessages: false,
-      draftingMessages: false,
-      error: null,
-  }
-  export function messageReducer(state=initState, action){
-      switch(action.payload){
-        case SENDING_MESSAGE:
-        return { ...state , sendingMessage: true, error: null}
-        case SUCCESS_SENDING_MESSAGE:
-        return {...state, sendingMessage: false, messages: [...state.messages, action.payload]}
-        case FAIL_SENDING_MESSAGE:
-        return {...state, error:action.payload}
-        case GETTING_MESSAGES:
-        return {...state, gettingMessages: true, error: null}
-        case SUCCESS_GETTING_MESSAGES: 
-        return {...state, gettingMessages: false, messages: action.payload}
+const initState = {
+  messages: [],
+  sendingMessage: false,
+  gettingMessages: false,
+  draftingMessages: false,
+  error: null
+};
+export function messageReducer(state = initState, action) {
+  switch (action.payload) {
+    case SENDING_MESSAGE:
+      return { ...state, sendingMessage: true, error: null };
+    case SUCCESS_SENDING_MESSAGE:
+      return {
+        ...state,
+        sendingMessage: false,
+        messages: [...state.messages, action.payload]
+      };
+    case FAIL_SENDING_MESSAGE:
+      return { ...state, error: action.payload };
+    case GETTING_MESSAGES:
+      return { ...state, gettingMessages: true, error: null };
+    case SUCCESS_GETTING_MESSAGES:
+      return { ...state, gettingMessages: false, messages: action.payload };
+    case FAIL_GETTING_MESSAGES:
+      return { ...state, gettingMessages: false, messages: action.payload };
+    case DRAFTING_MESSAGE:
+      return { ...state, draftingMessages: true, error: null };
+    case SUCCESS_DRAFTING_MESSAGE:
+      return { ...state, draftingMessages: false, messages: action.payload };
+    case FAIL_DRAFTING_MESSAGE:
+      return { ...state, draftingMessages: false, error: action.payload };
     default:
-        return state
-      }
+      return state;
   }
+}
