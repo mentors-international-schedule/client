4d57e53870b795363df41cfbce665c9df8b8f06c
guillsav <guillsav@gmail.com>
2019-03-13 13:22:29 -0400

    Carousel JS & LESS  code refactored to use Classlist

--- landing/img/Mentors International _ Home_files/carousel.js
+++ landing/img/Mentors International _ Home_files/carousel.js
@@ -6,11 +6,11 @@ class Carousel {
     this.nextBtn = document.querySelector('.next');
     this.nextBtn.style.zIndex = 50;
     this.storiesRefs = {
-      stories: document.querySelectorAll('.stories-display')
+      stories: document.querySelectorAll('.stories')
     };
     this.index = 0;
-
-    this.storiesRefs.stories[this.index].classList.toggle('stories');
+    this.storiesRefs.stories[this.index].classList.add('stories');
+    this.storiesRefs.stories[this.index].classList.remove('stories-display');
 
     this.nextBtn.addEventListener('click', () => this.next());
     this.previousBtn.addEventListener('click', () => this.previous());
@@ -20,25 +20,27 @@ class Carousel {
     if (this.index >= this.storiesRefs.stories.length) {
       this.index = 0;
     }
-    TweenMax.from(this.storiesRefs.stories, 0.5, {x: '50px', opacity: '0'});
-    TweenMax.to(this.storiesRefs.stories, 0.5, {x: '0px', opacity: '1'});
+    // TweenMax.from(this.storiesRefs.stories, 0.5, {x: '50px', opacity: '0'});
+    // TweenMax.to(this.storiesRefs.stories, 0.5, {x: '0px', opacity: '1'});
 
     this.storiesRefs.stories.forEach(story => {
-      story.classList.remove('stories');
+      story.style.display = 'none';
     });
-    this.storiesRefs.stories[this.index].classList.toggle('stories');
+    this.storiesRefs.stories[this.index].style.display = 'flex';
+    this.storiesRefs.stories[this.index].style.justifyContent = 'space-evenly';
   }
   previous() {
     this.index--;
     if (this.index < 0) {
       this.index = this.storiesRefs.stories.length - 1;
     }
-    TweenMax.from(this.storiesRefs.stories, 0.5, {x: '-50px', opacity: '0'});
-    TweenMax.to(this.storiesRefs.stories, 0.5, {x: '0px', opacity: '1'});
+    // TweenMax.from(this.storiesRefs.stories, 0.5, {x: '-50px', opacity: '0'});
+    // TweenMax.to(this.storiesRefs.stories, 0.5, {x: '0px', opacity: '1'});
     this.storiesRefs.stories.forEach(story => {
-      story.classList.remove('stories');
+      story.style.display = 'none';
     });
-    this.storiesRefs.stories[this.index].classList.toggle('stories');
+    this.storiesRefs.stories[this.index].style.display = 'flex';
+    this.storiesRefs.stories[this.index].style.justifyContent = 'space-evenly';
   }
 }
 
